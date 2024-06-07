32aeff6f19998facab02e98b450e16e8d03158bb
Jeevy Seguerra <jeevyseguerra6@gmail.com>
2024-06-07 10:26:29 +0800

    EXPENSE TRACKER

--- src/Dashboard/ExpenseReport.java
+++ src/Dashboard/ExpenseReport.java
@@ -7,6 +7,7 @@ package Dashboard;
 import EP.Db;
 import EP.UserSession;
 import java.awt.Color;
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -39,7 +40,7 @@ public class ExpenseReport extends javax.swing.JFrame {
         expensetable2 = new javax.swing.JTable();
         jLabel1 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
-        jLabel3 = new javax.swing.JLabel();
+        exlabel = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         exminus = new javax.swing.JLabel();
         exexit = new javax.swing.JLabel();
@@ -80,20 +81,18 @@ public class ExpenseReport extends javax.swing.JFrame {
 
         jPanel2.setForeground(new java.awt.Color(255, 255, 255));
 
-        jLabel3.setFont(new java.awt.Font("Trebuchet MS", 1, 18)); // NOI18N
-        jLabel3.setText("jLabel3");
+        exlabel.setFont(new java.awt.Font("Trebuchet MS", 1, 18)); // NOI18N
+        exlabel.setText("jLabel3");
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 206, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 15, Short.MAX_VALUE))
+            .addComponent(exlabel, javax.swing.GroupLayout.DEFAULT_SIZE, 221, Short.MAX_VALUE)
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 37, Short.MAX_VALUE)
+            .addComponent(exlabel, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 37, Short.MAX_VALUE)
         );
 
         jLabel2.setFont(new java.awt.Font("Trebuchet MS", 1, 18)); // NOI18N
@@ -182,6 +181,7 @@ public class ExpenseReport extends javax.swing.JFrame {
 
     public void fetchExpenses(){
         int userId = UserSession.getUserID(); 
+        BigDecimal totalExpense = BigDecimal.ZERO;
     
         try {           
             pst = con.prepareStatement("SELECT * FROM expenses WHERE userid = ?");
@@ -201,10 +201,12 @@ public class ExpenseReport extends javax.swing.JFrame {
                     rs.getDate("date")
                 };
                 expenseTableModel.addRow(rowData); 
+                totalExpense = totalExpense.add(rs.getBigDecimal("amount"));
             }
 
             rs.close();
             pst.close();
+            exlabel.setText("PHP " + totalExpense);
         } catch (SQLException ex) {
             Logger.getLogger(Dashboard.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -243,11 +245,11 @@ public class ExpenseReport extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel exexit;
+    private javax.swing.JLabel exlabel;
     private javax.swing.JLabel exminus;
     private javax.swing.JTable expensetable2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane2;
